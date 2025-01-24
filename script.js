03bb2b7b8d21376f5f5b5ed9ac0171360d20c2db
DuilioAzevedo <duilio_uei@hotmail.com>
2025-01-24 02:12:36 -0300

    add funcao contador

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+const conteiner = document.getElementById("conteiner");
+const botaoContar = document.getElementById("botaoContar");
+let contador = 0;
+
+
+botaoContar.addEventListener("click", function() {
+  contador= contador+1;
+  const valor = document.getElementById("valor");
+  valor.textContent = contador;
+
+});
